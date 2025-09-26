d5cee17ae298aa533c219725cc2c30fa08208ad4
Matheus Quirino <quirinojulio77@gmail.com>
2025-09-26 07:50:07 -0300

    commit diário

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,23 @@
 const hamburguer = document.querySelector(".hamburguer-menu");
 const nav = document.querySelector("nav")
 
+
 hamburguer.addEventListener("click", () => {
     hamburguer.classList.toggle("open");
     nav.classList.toggle("open")
 });
+
+let counter = 1;
+document.getElementById("radio1").checked = true;
+
+function nextimage() {
+    counter++;
+    if(counter > 4){
+        counter = 1;
+    }
+    document.getElementById("radio" + counter).checked = true;
+}
+
+// chama a função a cada 2 segundos
+setInterval(nextimage, 5000);
+
