f09c50e88289ed78e41441a480d294773e37a084
Matheus Quirino <quirinojulio77@gmail.com>
2025-09-26 16:08:40 -0300

    banco de dados e config.php

---
+++ Home/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "Techforge_db";
+
+$conexao = mysqli_connect($hostname, $username, $password, $database);
+
+if (!$conexao) {
+   echo "Erro na conexão com o banco de dados: " . mysqli_connect_error();
+}
+
+else {
+   echo "Conexão bem-sucedida!";
+}
+
+?>
